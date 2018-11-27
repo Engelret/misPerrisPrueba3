e47a5e587f4b431cf4159c53f0997c51d98ad174
Engel <eng.retamal@alumnos.duoc.cl>
2018-11-27 15:01:45 -0300

    migracion a base de datos elephant

--- misPerrisDjango3/settings.py
+++ misPerrisDjango3/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'misPerrisDjango3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vdkptgvf',
+        'USER': 'vdkptgvf',
+        'PASSWORD': 'n1c8zM5i89tFTZmaVUWS_pBcJeablQWI',
+        'HOST': 'pellefant.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
