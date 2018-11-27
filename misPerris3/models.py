6e13a63361bf0cb76f7ca885692bbee06425a6be
Engel <eng.retamal@alumnos.duoc.cl>
2018-11-27 00:47:42 -0300

    cambios modelos y login

--- misPerris3/models.py
+++ misPerris3/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    run = models.CharField(max_length = 10)
+    correo = models.CharField(max_length = 60)
+    nombre = models.CharField(max_length = 100)
+    fechaNac = models.CharField(max_length = 10)
+    telefono = models.BigIntegerField()
+    nombreUsuario = models.CharField(max_length = 40, unique = True)
+    contraseñaUsuario = models.CharField(max_length = 50)
+    region = models.CharField(max_length = 60)
+    comuna = models.CharField(max_length = 60)
+    vivienda = models.CharField(max_length = 60)
+
+    def __str__(self):
+        return "usuario"
+
+class Rescatado(models.Model):
+    nombre = models.CharField(max_length = 60)
+    raza = models.CharField(max_length = 40)
+    foto = models.ImageField(upload_to = 'fotos/')
+    descripcion = models.CharField(max_length = 200)
+    estado = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return "rescatados"
--- misPerris3/views.py
+++ misPerris3/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Usuario
+
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html',{'nombre':"Engel",'elementos':["uno","dos","tres","cuatro"]})
+
+
+def registroPersona(request):
+    return render(request,'registroPersona.html',{})
+
+def crearPersona(request):
+    nombres = request.POST.get('nombres','')
+    apellidos = request.POST.get('apellidos','')
+    email = request.POST.get('email','')
+    contrasenia = request.POST.get('contrasenia','')
+    reContrasenia = request.POST.get('reContrasenia','')
+
+    persona = Persona(nombres = nombres,apellidos = apellidos,email=email,contrasenia=contrasenia,reContrasenia=reContrasenia)
+    persona.save()
+    return HttpResponse("nombre : "+nombres+" apellidos : "+apellidos+" email : "+email+" contraseña : "+contrasenia+" reContraseña : "+reContrasenia)
+
+def login(request):
+    return render(request,'login.html',{})
