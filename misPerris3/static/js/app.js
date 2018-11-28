7348910567cee4e2f2f097e6b83434d5ce8e053b
Engel <eng.retamal@alumnos.duoc.cl>
2018-11-28 00:06:14 -0300

    añadida descripción de perros js

--- misPerris3/static/js/app.js
+++ misPerris3/static/js/app.js
@@ -102,4 +102,80 @@ var chile = {
         "region": "Región Metropolitana de Santiago",
         "comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
     }]
+    
 }
+
+
+
+$(document).ready(function(){
+    $("#btnOso").popover({content:"Raza: boyero de Berna o boyero de montaña bernés <br> Descripcion: es un perro "+
+     "muy popular actualmente ya que es un excelente perro familiar que además se desempeña muy"+
+      "bien en algunas labores como la búsqueda y rescate y el apoyo en terapias para adultos y"+
+      " niños. Es sin duda un perro excelente en muchos sentidos"
+    , html:true})
+})
+
+
+
+$(document).ready(function(){
+    $("#btnMaya").popover({content:"Raza: abrador retriever <br> Descripción: es una de las razas de perros más populares del mundo,"+
+    "teniendo en cuenta la cantidad de ejemplares registrados.  Aunque "+
+    "por lo general hablamos de un perro bondadoso, dócil y muy paciente, debemos saber que es"+
+    " también un amigo incansable que va a necesitar largas horas de ejercicio y juegos para mantenerse en forma y evitar el sobrepeso,"+
+    "un problema común en esta raza."
+    , html:true})
+})
+
+
+
+$(document).ready(function(){
+    $("#btnWifi").popover({content:"Raza: border collie <br> Descripción: es considerado el perro más inteligente del mundo, según Stanley Coren, el profesor e"+
+    " investigador neuropsicológico del famoso libro La inteligencia de los perros, publicado en 1994."+
+    " border collie es una raza canina que destaca por poseer una amplia capacidad de aprendizaje en distintos ámbitos: obediencia canina"+ 
+    " básica, avanzada, habilidades caninas, pastoreo o Agility entre otros."
+    , html:true})
+})
+
+
+
+$(document).ready(function(){
+    $("#btnChocolate").popover({content:"Raza: pointer inglés <br> Descripción:  es, con diferencia, uno de los perros cazadores más conocidos, también llamado (pointer)"+
+    ", simplemente. Se trata de un perro activo y dinámico, que se adaptará de maravilla si tiene a su lado un propietario activo y deportista, que le"+
+    "ayude a sacar el máximo partido de sus cualidades."+" si te estás planteando adoptar un perro pointer inglés cachorro o adulto será muy"+
+    " recomendable informarte previamente acerca de los cuidados que necesita, el adiestramiento que debe recibir o la alimentación a proporcionar, siempre en"+
+    " relación a su nivel de actividad física. "
+    , html:true})
+})
+
+
+
+$(document).ready(function(){
+    $("#btnPexel").popover({content:"Raza: El yorkshire terrier <br> Descripción: no es solo uno de los perros más "+
+    "pequeños del mundo, si no uno de los más acogidos por su tamaño y ternura. "+
+    "Es una de las mascotas más populares.  Saber acerca de su alimentación, el tamaño que alcanzará de adulto o cómo llevar a cabo su"+
+    "adiestramiento son algunas de las cosas básicas que debes tener claras antes de adoptar a uno, recuerda que un perro puede acompañarte muchos años y que"+
+    " debes ser muy responsable a la hora de acogerlo."
+    , html:true})
+})
+
+
+
+$(document).ready(function(){
+    $("#btnBigotes").popover({content:"Raza: airedale terrier <br> Descripción: es un perro alegre, confiado, "+
+    "valiente e inteligente. Suele ser amigable con la gente y también con otros perros, "+
+    "pero necesita una buena socialización desde cachorro. Este perro es impulsivo y tiende a "+
+    "presentar intensas conductas de presa. Por tanto, es necesario entrenarlo desde cachorro,"+
+     "aunque siempre será preferible un adiestramiento sin maltratos ni intentos de dominación"
+    , html:true})
+})
+
+
+
+
+$(document).ready(function(){
+    $("#btnLuna").popover({content:"Raza:perro corso italiano <br> Descripción: es también conocido como cane corso o mastín "+
+    "italiano y es sin duda, junto al mastín napolitano"+
+    " una de las razas de perros molosos tipo dogo italianos más impactantes. Su nombre"+
+    " deriva del latín (cohors) que significa (protector, guardián del corral)."
+    , html:true})
+})
